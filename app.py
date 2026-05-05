a654d654d2c8255e968ceaf8cbaecdd9746fd001
Lucas Barbosa Trindade <lucas.barbosa.luca1002@gmail.com>
2026-05-05 10:22:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
-def hello():
-    return "<h1>🚀 ARTEMIS VIVO! Funcionando 100%!</h1>"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
