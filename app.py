03569ced8f138d4b38506e1fb3af2706ccea4cb2
lucas <lucas.barbosa.luca1002@gmail.com>
2026-05-05 09:40:29 -0300

    Fix Azure 500 error

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+@app.route('/<path:path>')
+def catch_all(path=''):
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/health')
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
